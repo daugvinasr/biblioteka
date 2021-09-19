d08b302a5a08679cf0ad171b79347160757a916e
ormsher <osmis100@gmail.com>
2021-09-19 14:51:14 +0300

    first commit

---
+++ resources/views/start.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <title>Biblioteka</title>
+    <link href="css/style.css" rel="stylesheet"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="grid-container">
+        <div class="grid-item grid-item-1">
+            <h1 class="grid1h1">Biblioteka</h1>
+            <h2 class="grid1h1">Čia galite rasti knygas apie visus CSS gradient standartus</h2>
+        </div>
+        <div class="grid-item grid-item-2">
+            <div class="bar">
+                <a class="barall" href="#">Pagrindinis</a>
+                <a class="barall" href="#">Autoriai</a>
+                <a class="barall" href="#">Knygos</a>
+                <a class="barall" href="#">Paimtos knygos</a>
+                <a class="barall" href="#">Studentai</a>
+                <a class="barall" href="#">Tipai</a>
+            </div>
+        </div>
+        <div class="grid-item grid-item-3">
+            <div class="grid3text">
+                <h2>Lorem ipsum dolor</h2>
+                <p>Molestias quod optio perspiciatis vitae accusamus voluptatem laudantium maiores at nulla, distinctio ad commodi aut id debitis rerum. Tempore rerum beatae, laboriosam expedita esse sit ipsa, illo ipsum accusantium qui earum quasi minus dolor cupiditate dicta aut a! Enim quae, quis distinctio voluptates est itaque, animi amet consectetur praesentium expedita molestiae dignissimos vitae? Distinctio molestiae explicabo cumque quidem impedit quisquam tenetur nisi est officiis vitae suscipit, rem nihil ipsum!</p>
+                <p>&nbsp;</p>
+                <h2>Amet consectetur</h2>
+                <p>Ipsum dolor sit amet consectetur, adipisicing elit. Corrupti cupiditate libero, molestias quod optio perspiciatis vitae accusamus voluptatem laudantium maiores at nulla, distinctio ad commodi aut id debitis rerum. Tempore rerum beatae, laboriosam expedita esse sit ipsa, illo ipsum accusantium qui earum quasi minus dolor cupiditate dicta aut a! Enim quae, quis distinctio voluptates est itaque, animi amet consectetur praesentium expedita molestiae dignissimos vitae? Distinctio molestiae explicabo cumque quidem impedit quisquam tenetur nisi est officiis vitae suscipit, rem nihil ipsum!</p>
+                <p>&nbsp;</p>
+                <h2>Corrupti cupiditate</h2>
+                <p>Dolor sit amet consectetur, adipisicing elit. Corrupti cupiditate libero, molestias quod optio perspiciatis vitae accusamus voluptatem laudantium maiores at nulla, distinctio ad commodi aut id debitis rerum. Tempore rerum beatae, laboriosam expedita esse sit ipsa, illo ipsum accusantium qui earum quasi minus dolor cupiditate dicta aut a! Enim quae, quis distinctio voluptates est itaque, animi amet consectetur praesentium expedita molestiae dignissimos vitae? Distinctio molestiae explicabo cumque quidem impedit quisquam tenetur nisi est officiis vitae suscipit, rem nihil ipsum!</p>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
